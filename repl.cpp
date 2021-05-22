d355155a7e6ec88667b8acd3a5bcb7722e7ed76c
hannu.eloranta <oh2lhe@gmail.com>
2021-05-22 22:29:43 +0300

    added Number and Label classes

--- repl.cpp
+++ repl.cpp
@@ -13,17 +13,17 @@ public:
 
 class Number : public SExpression
 {
-    char c;
+    int value;
 public:
     static SExpression* Parse();
-    Number(char c) : c(c) {}
+    Number(int value) : value(value) {}
     SExpression* Eval()
     {
         return this;
     }
     void Print()
     {
-        std::cout << "number\n";
+        std::cout << value << "\n";
     }
 };
 
@@ -45,24 +45,31 @@ public:
 
 class Label : public SExpression
 {
-    char c;
+    std::string label;
 public:
     static SExpression* Parse();
-    Label(char c) : c(c) {}
+    Label(std::string label) : label(label) {}
     SExpression* Eval()
     {
         return this;
     }
     void Print()
     {
-        std::cout << "label\n";
+        std::cout << label << "\n";
     }
 };
 
 SExpression* SExpression::Parse()
 {
     char c;
-    std::cin >> c;
+    std::cin.unsetf(std::ios_base::skipws);
+
+    do
+    {
+        std::cin >> c;
+    }
+    while (std::isspace(c));
+
     std::cin.putback(c);
 
     if (std::isdigit(c))
@@ -82,8 +89,20 @@ SExpression* SExpression::Parse()
 SExpression* Number::Parse()
 {
     char c;
+    std::string s;
     std::cin >> c;
-    return new Number(c);
+    while (std::isdigit(c))
+    {
+        s.push_back(c);
+        std::cin >> c;
+    }
+    if (std::isspace(c) || c == ')')
+    {
+        std::cin.putback(c);
+        return new Number(stoi(s));
+    }
+    
+    return new Label("error");
 }
 
 SExpression* List::Parse()
@@ -95,7 +114,7 @@ SExpression* List::Parse()
 
 SExpression* Label::Parse()
 {
-    char c;
+    std::string c;
     std::cin >> c;
     return new Label(c);
 }
